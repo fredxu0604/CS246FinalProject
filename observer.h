8ed5e64b28477ccedb13382c2ce3bb0eaf487203
j249tang <j249tang@uwaterloo.ca>
2023-04-02 02:41:50 -0400

    add observer and subject abstract classes

---
+++ observer.h
@@ -0,0 +1,13 @@
+#ifndef __OBSERVER_H__
+#define __OBSERVER_H__
+
+template <typename InfoType, typename StateType> class Subject;
+class Cell;
+
+template <typename InfoType, typename StateType> class Observer {
+ public:
+  virtual void notify(Subject<InfoType, StateType> &whoFrom) = 0; // pass the >
+  virtual ~Observer() = default;
+};
+
+#endif
---
+++ subject.h
@@ -0,0 +1,35 @@
+#ifndef __SUBJECT_H__
+#define __SUBJECT_H__
+#include <vector>
+
+template <typename InfoType, typename StateType> class Observer;
+
+template <typename InfoType, typename StateType> class Subject {
+  std::vector<Observer<InfoType, StateType>*> observers;
+  StateType state;
+ protected:
+  void setState(StateType newS);
+ public:
+  void attach(Observer<InfoType, StateType> *o);  
+  void notifyObservers();
+  virtual InfoType getInfo() const = 0;
+  StateType getState() const;
+};
+
+template <typename InfoType, typename StateType>
+void Subject<InfoType, StateType>::attach(Observer<InfoType, StateType> *o) {
+  observers.emplace_back(o);
+}
+
+template <typename InfoType, typename StateType>
+void Subject<InfoType, StateType>::notifyObservers() {
+  for (auto &ob : observers) ob->notify(*this);
+}
+
+template <typename InfoType, typename StateType>
+void Subject<InfoType, StateType>::setState(StateType newS) { state = newS; }
+
+template <typename InfoType, typename StateType>
+StateType Subject<InfoType, StateType>::getState() const { return state; }
+
+#endif
