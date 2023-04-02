c40795ba9ca743759284d5593641d6bc43be4a5d
j249tang <j249tang@uwaterloo.ca>
2023-04-02 07:13:54 -0400

    update Player interface

--- src/player.h
+++ src/player.h
@@ -20,15 +20,28 @@ class Player: public Subject<PlayerInfo, State> {
 
     PlayerInfo getInfo() const override;
 
-    void declareBankruptcy() const;
+    void declareBankruptcy() const; // player declares bankruptcy
 
-    bool buyImprovement(std::string propertyName);
+    bool buyImprovement(std::string propertyName);  // returns true if purchase is successful,
+                                                    //   false otherwise.
+                                                    //   THROWS EXN if insufficient funds
     
-    bool sellImprovement(std::string propertyName);
+    bool sellImprovement(std::string propertyName); // similar to above
 
-    bool mortgage(std::string propertyName);
+    bool mortgage(std::string propertyName); // returns true if mortgage successful,
+                                            //    false otherwise
 
-    bool unmortgage(std::string propertyName);
+
+    bool unmortgage(std::string propertyName); // similar to above except it THROWS EXN if insufficient funds
+
+    void roll(std::vector<Square> allSquares); // Let's have the player roll and travel for themselves
+                                              //    instead, and have the player report to Game
+                                              //    with its new location
+
+    bool makePayment(size_t amount); // Returns true if the player has enough money to make the payment
+                                    //   The player can declare bankruptcy if this returns false
+    
+    void moveTo(const Square &newLocation); // moves the player to a new square
 
     
 
