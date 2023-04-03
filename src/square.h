b97731a4a8a9d97558443cedebf9b34a8cd01e2d
fredxu <69193140+fredxu0604@users.noreply.github.com>
2023-04-03 16:46:28 -0400

    add square.h

--- src/square.h
+++ src/square.h
@@ -1,8 +1,23 @@
 #ifndef __SQUARE_H__
 #define __SQUARE_H__
+#include <string>
+#include <vector>
+#include "player.h"
+using namespace std;
 
-class Square; // replace this with ur actual Square implementation
-             //    (or replace this entire file if u already implemented it)
-
-
+class Square {
+    protected:
+    string name;
+    vector<Player*> visitors;
+    public:
+    Square(string name);
+    string getName() const;
+    virtual ~Square() = 0;
+    virtual bool isProperty() = 0;
+    virtual bool isOwned() = 0;
+    virtual Player* getOwner() = 0;
+    void addVisitor(Player* player);
+    void removeVisitor(Player* player);
+    vector<Player*> getVisitors() const;
+};
 #endif
