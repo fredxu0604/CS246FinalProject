2cc2f710745018d6d094a098be4b9829b975ae64
fredxu <69193140+fredxu0604@users.noreply.github.com>
2023-04-03 20:53:54 -0400

    add squareinfo.h

--- src/squareinfo.h
+++ src/squareinfo.h
@@ -1,6 +1,15 @@
 #ifndef __SQUAREINFO_H__
 #define __SQUAREINFO_H__
+#include <string>
+#include "player.h"
 // Whoever is implementing Square plz implement this too.
-struct SquareInfo;
+struct SquareInfo {
+    string name;
+    bool isProperty;
+    bool isOwned;
+    size_t cost;
+    vector<Player*> visitors;
+    Player* owner;
+};
 
 #endif
