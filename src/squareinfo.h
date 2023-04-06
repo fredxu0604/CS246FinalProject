5e3ad83e6ffa43157b9e9c3fa6d52579262acb2d
fredxu <69193140+fredxu0604@users.noreply.github.com>
2023-04-06 00:47:21 -0400

    update squareinfo

--- src/squareinfo.h
+++ src/squareinfo.h
@@ -1,15 +1,20 @@
 #ifndef __SQUAREINFO_H__
 #define __SQUAREINFO_H__
 #include <string>
-#include "player.h"
+class Player;
 struct SquareInfo {
     string name;
     bool isProperty;
-    bool isAcedemic;
+    PropertyType type;
     bool isOwned;
+    bool isMortgaged;
     size_t cost;
     Player* owner;
-    unsigned int numImprove;
+    size_t numImprove;
+    size_t improvementCost;
+    size_t mortgageLoan;
+    size_t unmortgageCost;
+    size_t improvementRefund;
 };
 
 #endif
