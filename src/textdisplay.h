7919ba0bb5cb05b06ca60dd95529c6a8849bfeb7
wwwlyclxn <56846930+wwwlyclxn@users.noreply.github.com>
2023-04-08 03:09:43 -0400

    Update textdisplay.h
    add a class Board *game, a 2D vector to store the Board.txt file and a public method TextDisplay

--- src/textdisplay.h
+++ src/textdisplay.h
@@ -5,16 +5,26 @@
 #include "boardinfo.h"
 #include "command.h"
 
+
+
 class Game;
+class Board;
 
 class TextDisplay {
+    
+    Board *game;
+    
+    std::vector<std::vector<char>> board;// 2D vector to store the Board.txt file
 
-    public:
+public:
+    TextDisplay(Board *game);
+    
     void print(std::ostream &out, const BoardInfo &board, std::string message = ""); // print the board and message (if exists) in this version
-
+    
     void print(std::ostream &out, std::string message = ""); // if message is "" then return (though that should never happen)
-
+    
     Command readCommand(); // reads a command from stdin into a Command object. see command.h for info
 };
 
 #endif
+
