566c7ec9cd221d71c8d17752fd6280a6579f2286
fredxu <69193140+fredxu0604@users.noreply.github.com>
2023-04-10 01:41:24 -0400

    add main

--- src/main.cc
+++ src/main.cc
@@ -10,7 +10,34 @@ int main(int argc, char **argv) {
         std::cout << "Wrong number of arguments" << endl;
         return 1;
     }
+    bool is_testing = false;
+    bool is_loading = false;
+    std::string loadName;
+    for (int i = 1; i < argc; i++) {
+        if (flagTesting.compare(argv[i])) {
+            is_testing = true;
+        }
+        if (flagLoad.compare(argv[i])) {
+            is_loading = true;
+            loadName += argv[i+1];
+        }
 
-    
-    
+    // Initialize Game object with testing flag
+    Game game(is_testing);
+
+    if (is_loading) {
+        // Load game state from file
+        game.loadFromFile(loadName + ".txt");
+    } else {
+        // Start a new game
+        game.newGame();
+    }
+
+    // Run game loop
+    game.runGameLoop();
+
+    // Save game state to file
+    game.saveToFile(loadName + ".txt");
+
+    return 0;
 }
